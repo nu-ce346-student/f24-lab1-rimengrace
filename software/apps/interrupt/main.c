f1b01a226f80c80a6ef98edf082010c58cd2784b
Rimen Jenhani <rimenjenhani2026@u.northwestern.edu>
2024-10-04 14:54:18 -0500

    first lab

--- software/apps/interrupt/main.c
+++ software/apps/interrupt/main.c
@@ -25,14 +25,19 @@ void software_interrupt_trigger(void) {
 void SWI1_EGU1_IRQHandler(void) {
   // Clear interrupt event
   NRF_EGU1->EVENTS_TRIGGERED[0] = 0;
+  printf("SOftware interrupt triggered");
+  nrf_delay_ms(5000);
+  printf("SOftware interrupt ended");
 
   // Implement me
+
 }
 
 void GPIOTE_IRQHandler(void) {
   // Clear interrupt event
   NRF_GPIOTE->EVENTS_IN[0] = 0;
-
+  printf("GPIOTE interrupt triggered");
+  nrf_delay_ms(1000);
   // Implement me
 }
 
@@ -44,11 +49,18 @@ int main(void) {
   //    where the register name in all caps goes after the arrow.
   //    For example, NRF_GPIOTE->CONFIG[0]
   // Add code here
+  NRF_GPIOTE->INTENSET=1;
+  NRF_GPIOTE->CONFIG[0]=  0x10E01;
+  NVIC_EnableIRQ( GPIOTE_IRQn );
+  NVIC_SetPriority( GPIOTE_IRQn,  0);
 
 
   // Second task. Trigger a software interrupt
   // Use the software_interupt_* functions defined above
   // Add code here
+  NVIC_SetPriority( SWI1_EGU1_IRQn,  1);
+  software_interrupt_init();
+  software_interrupt_trigger();
 
 
   // loop forever
