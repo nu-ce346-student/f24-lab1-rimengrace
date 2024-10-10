843d8be856d1d49311c60ed3714a4bdde0368972
Rimen Jenhani <rimenjenhani2026@u.northwestern.edu>
2024-10-10 15:37:42 -0500

    second commit

--- software/apps/gpio/gpio.c
+++ software/apps/gpio/gpio.c
@@ -1,9 +1,118 @@
+// #include "gpio.h"
+#include <stdio.h>
+
+// typedef struct{
+//   // Step 3:
+//   // Add register definitions here
+//   uint32_t unused_A [321];
+//   uint32_t OUT;
+//   uint32_t OUTSET;
+//   uint32_t OUTCLR;
+//   uint32_t IN;
+//   uint32_t DIR;
+//   uint32_t DIRSET;
+//   uint32_t DIRCLR;
+//   uint32_t LATCH;
+//   uint32_t DETECTMODE;
+//   uint32_t unused_B[118];
+//   uint32_t PIN_CNF[32];
+  
+// } gpio_reg_t;
+
+// volatile gpio_reg_t* port_zero=  (gpio_reg_t*) (0x50000000);
+// volatile gpio_reg_t* port_one=  (gpio_reg_t*) (0x50000300);
+
+
+// // Inputs: 
+// //  gpio_num - gpio number 0-31 OR (32 + gpio number)
+// //  dir - gpio direction (INPUT, OUTPUT)
+// void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
+//   // Implement me
+//   // This function should configure the pin as an input/output
+//   // Hint: Use proper PIN_CNF instead of DIR
+//   if (dir == 0){
+//     port_zero->PIN_CNF[gpio_num] = 0b0;
+//     port_one->PIN_CNF[gpio_num] = 0b0;
+//   }
+//   else if (dir == 1){
+//     port_zero->PIN_CNF[gpio_num] = 0b11;
+//     port_one->PIN_CNF[gpio_num] = 0b11;
+    
+//   }
+// }
+
+// // Inputs: 
+// //  gpio_num - gpio number 0-31 OR (32 + gpio number)
+// void gpio_set(uint8_t gpio_num) {
+//   // Implement me
+//   // This function should make the pin high
+//   // It can assume that the pin has already been configured
+//   port_zero->OUTSET = (port_zero->OUTSET & 0b1) << gpio_num;
+//   port_one->OUTSET = (port_one->OUTSET & 0b1) << gpio_num;
+// }
+
+
+// // Inputs: 
+// //  gpio_num - gpio number 0-31 OR (32 + gpio number)
+// void gpio_clear(uint8_t gpio_num) {
+//   // Implement me
+//   // This function should make the pin low
+//   // It can assume that the pin has already been configured
+//   port_zero->OUTCLR = (port_zero->OUTCLR & 0b1) << gpio_num;
+//   port_one->OUTCLR = (port_one->OUTCLR & 0b1) << gpio_num;
+// }
+
+// // Inputs: 
+// //  gpio_num - gpio number 0-31 OR (32 + gpio number)
+// // Output:
+// //  bool - pin state (true == high)
+// bool gpio_read(uint8_t gpio_num) {
+//   // Implement me
+//   // This function should read the value from the pin
+//   // It can assume that the pin has already been configured
+//   bool high= (port_zero->IN >> gpio_num) && 0b1;
+//   return high;
+// }
+
+// // prints out some information about the GPIO driver. Can be called from main()
+// void gpio_print(void) {
+//   // Use this function for debugging purposes
+//   // For example, you could print out struct field addresses
+//   // You don't otherwise have to write anything here
+
+//   printf("Address of PORT0: OUT is %p\n", &(port_zero->OUT));
+//   printf("Address of PORT0: OUTCLR is %p\n", &(port_zero->OUTCLR));
+//   printf("Address of PORT0: DIRSET is %p\n", &(port_zero->DIRSET));
+//   printf("Address of PORT0: DETECTMODE is %p\n", &(port_zero->DETECTMODE));
+//   printf("Address of PORT0: PIN_CRF[0] is %p\n", &(port_zero->PIN_CNF[0]));
+
+//   printf("Address of PORT1: OUT is %p\n", &(port_one->OUT));
+//   printf("Address of PORT1: OUTCLR is %p\n", &(port_one->OUTCLR));
+//   printf("Address of PORT1: DIRSET is %p\n", &(port_one->DIRSET));
+//   printf("Address of PORT1: DETECTMODE is %p\n", &(port_one->DETECTMODE));
+//   printf("Address of PORT1: PIN_CRF[0] is %p\n", &(port_one->PIN_CNF[0]));
+// }
+
 #include "gpio.h"
 
 typedef struct{
   // Step 3:
-  // Add register definitions here
+  uint32_t unused_A [321];
+  uint32_t OUT;
+  uint32_t OUTSET;
+  uint32_t OUTCLR;
+  uint32_t IN;
+  uint32_t DIR;
+  uint32_t DIRSET;
+  uint32_t DIRCLR;
+  uint32_t LATCH;
+  uint32_t DETECTMODE;
+  uint32_t unused_B[118];
+  uint32_t PIN_CNF[32];
 } gpio_reg_t;
+
+volatile gpio_reg_t* port_zero=  (gpio_reg_t*) (0x50000000);
+volatile gpio_reg_t* port_one=  (gpio_reg_t*) (0x50000300);
 
 // Inputs: 
 //  gpio_num - gpio number 0-31 OR (32 + gpio number)
@@ -12,6 +121,22 @@ void gpio_config(uint8_t gpio_num, gpio_direction_t dir) {
   // Implement me
   // This function should configure the pin as an input/output
   // Hint: Use proper PIN_CNF instead of DIR
+  if (gpio_num < 32) {
+    if (dir == 0){
+    port_zero ->PIN_CNF[gpio_num] = 0b0;
+    }
+    else if (dir == 1){
+    port_zero ->PIN_CNF[gpio_num] = 0b11;
+    }
+  }        
+  else {
+    if (dir == 0){
+      port_one ->PIN_CNF[gpio_num-32] = 0b0;
+    }
+    else if (dir == 1){
+      port_one ->PIN_CNF[gpio_num-32] = 0b11;
+    }
+  }
 }
 
 // Inputs: 
@@ -20,6 +145,11 @@ void gpio_set(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin high
   // It can assume that the pin has already been configured
+  if (gpio_num < 32) {
+        port_zero->OUTSET = (1 << gpio_num);
+  } else {
+   port_one->OUTSET = (1 << (gpio_num - 32));
+  }
 }
 
 // Inputs: 
@@ -28,6 +158,11 @@ void gpio_clear(uint8_t gpio_num) {
   // Implement me
   // This function should make the pin low
   // It can assume that the pin has already been configured
+  if (gpio_num < 32) {
+    port_zero->OUTCLR = (1 << gpio_num);
+  } else {
+    port_one->OUTCLR = (1 << (gpio_num - 32));
+  }
 }
 
 // Inputs: 
@@ -38,8 +173,11 @@ bool gpio_read(uint8_t gpio_num) {
   // Implement me
   // This function should read the value from the pin
   // It can assume that the pin has already been configured
-
-  return true;
+  if (gpio_num < 32) {
+   return (port_zero->IN >> gpio_num) && 0b1;
+  } else {
+    return (port_one->IN >> gpio_num) && 0b1;
+  }
 }
 
 // prints out some information about the GPIO driver. Can be called from main()
@@ -47,5 +185,16 @@ void gpio_print(void) {
   // Use this function for debugging purposes
   // For example, you could print out struct field addresses
   // You don't otherwise have to write anything here
-}
 
+  printf("Address of PORT0: OUT is %p\n", &(port_zero->OUT));
+  printf("Address of PORT0: OUTCLR is %p\n", &(port_zero->OUTCLR));
+  printf("Address of PORT0: DIRSET is %p\n", &(port_zero->DIRSET));
+  printf("Address of PORT0: DETECTMODE is %p\n", &(port_zero->DETECTMODE));
+  printf("Address of PORT0: PIN_CRF[0] is %p\n", &(port_zero->PIN_CNF[0]));
+
+  printf("Address of PORT1: OUT is %p\n", &(port_one->OUT));
+  printf("Address of PORT1: OUTCLR is %p\n", &(port_one->OUTCLR));
+  printf("Address of PORT1: DIRSET is %p\n", &(port_one->DIRSET));
+  printf("Address of PORT1: DETECTMODE is %p\n", &(port_one->DETECTMODE));
+  printf("Address of PORT1: PIN_CRF[0] is %p\n", &(port_one->PIN_CNF[0]));
+}
--- software/apps/gpio/main.c
+++ software/apps/gpio/main.c
@@ -21,8 +21,32 @@ int main(void) {
   // Control LED with raw MMIO
   // Microphone LED is P0.20 and active high
   // Add code here
-
+ 
+  // NRF_GPIO->DIRSET=0x100000;
+  // NRF_GPIO->OUTSET=0x100000;
   // loop forever
+  gpio_config(21,1);
+  gpio_config(22,1);
+  gpio_config(15,1);
+  gpio_config(24,1);
+  gpio_config(19,1);
+  gpio_config(28,1);
+  gpio_config(11,1);
+  gpio_config(31,1);
+  gpio_config(37,1);
+  gpio_config(30,1);
+
+  gpio_set(21);
+  gpio_set(22);
+  gpio_set(15);
+  gpio_set(24);
+  gpio_set(19);
+  gpio_clear(28);
+  gpio_clear(11);
+  gpio_clear(31);
+  gpio_clear(37);
+  gpio_clear(30);
+
   printf("Looping\n");
   while (1) {
 
@@ -30,7 +54,7 @@ int main(void) {
     // Button A is P0.14 and active low
     // Button B is P0.23 and active low
     // Add code here
-
+   
     nrf_delay_ms(100);
   }
 }
